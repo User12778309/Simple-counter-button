d625f163c0d3c92a83149e7e2a475c770272ad2f
User12778309 <sammy12778309@gmail.com>
2024-02-10 22:05:25 +0100

    Button js

---
+++ main.js
@@ -0,0 +1,34 @@
+function dark_mode_button()
+{
+
+    const elem1 = document.getElementById("circle")
+    const elem2 = document.getElementById("body")
+    const elem3 = document.getElementById("dark_mode_button")
+    const elem4 = document.getElementById("normal_mode_button")
+
+    elem1.style.backgroundColor = ("white")
+    elem2.style.backgroundColor = ("black")
+    elem3.style.backgroundColor = ("black")
+    elem3.style.color = ("white")
+    elem4.style.backgroundColor = ("black")
+    elem4.style.color = ("white")
+
+}
+
+function normal_mode_button()
+{
+
+    const elem1 = document.getElementById("circle")
+    const elem2 = document.getElementById("body")
+    const elem3 = document.getElementById("dark_mode_button")
+    const elem4 = document.getElementById("normal_mode_button")
+
+    elem1.style.backgroundColor = ("red")
+    elem2.style.backgroundColor = ("orange")
+    elem3.style.backgroundColor = ("white")
+    elem3.style.color = ("black")
+    elem4.style.color = ("black")
+    elem4.style.backgroundColor = ("white")
+
+
+}
