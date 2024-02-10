562715f84cd4febe86beaade327ef9683b8cf1f3
User12778309 <sammy12778309@gmail.com>
2024-02-11 00:20:22 +0100

    v3 compteur

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function dark_mode_button()
     const elem2 = document.getElementById("body")
     const elem3 = document.getElementById("dark_mode_button")
     const elem4 = document.getElementById("normal_mode_button")
+    const elem5 = document.getElementById("nombre")
 
     elem1.style.backgroundColor = ("white")
     elem2.style.backgroundColor = ("black")
@@ -12,6 +13,7 @@ function dark_mode_button()
     elem3.style.color = ("white")
     elem4.style.backgroundColor = ("black")
     elem4.style.color = ("white")
+    elem5.style.color = ("white")
 
 }
 
@@ -22,6 +24,7 @@ function normal_mode_button()
     const elem2 = document.getElementById("body")
     const elem3 = document.getElementById("dark_mode_button")
     const elem4 = document.getElementById("normal_mode_button")
+    const elem5 = document.getElementById("nombre")
 
     elem1.style.backgroundColor = ("red")
     elem2.style.backgroundColor = ("orange")
@@ -29,6 +32,12 @@ function normal_mode_button()
     elem3.style.color = ("black")
     elem4.style.color = ("black")
     elem4.style.backgroundColor = ("white")
+    elem5.style.color = ("black")
+}
 
-
+function add_nbr()
+{
+    let nbr = document.getElementById("nombre").innerHTML
+    nbr = parseInt(nbr) +1
+    document.getElementById("nombre").innerHTML = (nbr)
 }
